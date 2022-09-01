a2d9ee59b18a2758a0b07d37e679a01f45f513a1
Shabbir1065 <shabbir1065@gmail.com>
2022-09-01 18:46:37 -0400

    Finished the about section

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//NAVBAR CODE
 let header = document.querySelector('header');
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
@@ -17,4 +18,19 @@ darkmode.onclick = () => {
         darkmode.classList.replace('fa-sun', 'fa-moon');
         document.body.classList.remove('active');
     }
+}
+
+//ABOUT SECTION CODE
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove('active-link');
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove('active-tab');
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 }
